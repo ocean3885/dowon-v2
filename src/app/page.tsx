5996a5c2015bcb4aebeed364ca98f93c312c3edb
ocean3885 <ocean3885@naver.com>
2026-05-26 20:04:25 +0900

    PhilosophySection 위치 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,10 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <HeroSection />
+      <PhilosophySection />
       <CounselingPhilosophySection />
       <CounselingCategorySection />
       <BaziServiceSection />
-      <PhilosophySection />
       <NamingSection />
       <NamingInfoSection />
       <ReviewSection />
